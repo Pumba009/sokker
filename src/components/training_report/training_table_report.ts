f8689cae87437769b997e9709a4bd8eede57bdd9
Pumba09 <siemaelo925@gmail.com>
2026-02-15 18:08:52 +0100

    add color cell in table report

--- src/components/training_report/training_table_report.ts
+++ src/components/training_report/training_table_report.ts
@@ -3,10 +3,10 @@ import { IPlayer } from '../../types/interfaces';
 import { playerAttributesShortName, playerAttributesMapper } from '../../constants';
 
 export class TrainingTableReport<T extends IPlayer> {
-  private _data: T;
+  private _playerData: T;
 
-  public constructor(data: T) {
-    this._data = data;
+  public constructor(playerProgressHistory: T) {
+    this._playerData = playerProgressHistory;
   }
 
   public renderTrainingReportOnPage(raportType: ReportType = ReportType.Table) {
@@ -24,31 +24,32 @@ export class TrainingTableReport<T extends IPlayer> {
     layoutMainDiv.className = 'layout__main';
     mtpDiv.className = 'mtp-tab__table';
     trainingDiv.className = 'training-table';
-    tableDiv.className = 'table is-sticky';
+    tableDiv.className = 'is-sticky';
     tableRoot.className = 'table__root';
 
-    console.log(this._data);
-
     const thead: HTMLTableSectionElement = tableRoot.createTHead();
-    thead.appendChild(this.addHead({ key: 'data', value: 'data' }));
+    const row: HTMLTableRowElement = thead.insertRow();
+    row.appendChild(this.addHead({ key: 'data', value: 'data' }));
     playerAttributesShortName.forEach((playerAttributeKeyValuePair) => {
-      thead.appendChild(this.addHead(playerAttributeKeyValuePair));
+      row.appendChild(this.addHead(playerAttributeKeyValuePair));
     });
 
-    let i = this._data.progressHistory.length - 1;
-    this._data.progressHistory.reverse().forEach((playerStats) => {
-      const row: HTMLTableRowElement = tableRoot.insertRow();
-      this.addCell(row, 'data', this._data.updateDateTime[i].split('T')[0]);
-      i--;
+    const tbody: HTMLTableSectionElement = tableRoot.createTBody();
+    let weekIndex = this._playerData.progressHistory.length - 1;
+    for (weekIndex; weekIndex >= 0; weekIndex--) {
+      const row: HTMLTableRowElement = tbody.insertRow();
+      this.addFirstCell(row, this._playerData.updateDateTime[weekIndex].split('T')[0]);
 
+      const playerStats = this._playerData.progressHistory[weekIndex];
       playerAttributesMapper.forEach((playerAttributesMapper) => {
         this.addCell(
           row,
           playerAttributesMapper.value,
           playerStats[playerAttributesMapper.key].toString(),
+          weekIndex,
         );
       });
-    });
+    }
 
     tableDiv.appendChild(tableRoot);
     trainingDiv.appendChild(tableDiv);
@@ -62,7 +63,7 @@ export class TrainingTableReport<T extends IPlayer> {
   private addHead(attribute: { key: string; value: string }): HTMLTableCellElement {
     const th: HTMLTableCellElement = document.createElement('th');
     //const cell: HTMLTableCellElement = thead.insertCell();
-    th.className = `table__th table__th--${attribute.key} is-sortable is-sticky`;
+    th.className = `table__th table__th--${attribute.key} is-sortable`;
     const wrapedDiv = document.createElement('div');
     wrapedDiv.className = 'table__th-wrap';
     const dataDiv = document.createElement('div');
@@ -80,10 +81,26 @@ export class TrainingTableReport<T extends IPlayer> {
     return th;
   }
 
+  private addFirstCell(row: HTMLTableRowElement, datetime: string) {
+    const cell: HTMLTableCellElement = row.insertCell();
+    cell.className = `table__cell table__cell--date-time`;
+    const wrapedDiv = document.createElement('div');
+    wrapedDiv.className = 'table__cell-wrap table__cell-wrap--border-left';
+    const span = document.createElement('span');
+    const wrapedSpan = document.createElement('span');
+    wrapedSpan.className = 'growth-bg';
+    wrapedSpan.textContent = datetime;
+
+    span.appendChild(wrapedSpan);
+    wrapedDiv.appendChild(span);
+    cell.appendChild(wrapedDiv);
+  }
+
   private addCell(
     row: HTMLTableRowElement,
     playerAttributeName: string,
     playerAttributeValue: string,
+    index: number,
   ) {
     const cell: HTMLTableCellElement = row.insertCell();
     cell.className = `table__cell table__cell--${playerAttributeName}`;
@@ -91,24 +108,37 @@ export class TrainingTableReport<T extends IPlayer> {
     wrapedDiv.className = 'table__cell-wrap table__cell-wrap--border-left';
     const span = document.createElement('span');
     const wrapedSpan = document.createElement('span');
-    wrapedSpan.className = 'growth-bg';
+    wrapedSpan.className = this.colorCell(playerAttributeName, playerAttributeValue, index);
     wrapedSpan.textContent = playerAttributeValue;
 
     span.appendChild(wrapedSpan);
     wrapedDiv.appendChild(span);
     cell.appendChild(wrapedDiv);
   }
+
+  private colorCell(
+    playerAttributeName: string,
+    playerAttributeValue: string,
+    index: number,
+  ): string {
+    index = index - 1;
+    if (index <= 0) {
+      return 'growth-bg';
+    }
+
+    const lastWeekPlayerAttributes = this._playerData.progressHistory[index];
+    const attribute = playerAttributesMapper.find((kv) => kv.value == playerAttributeName)!.key; //[playerAttributeName as keyof IPlayerStats];
+    const lastWeekAttributeValue = lastWeekPlayerAttributes[attribute];
+
+    const playerAttributeValueNum = Number(playerAttributeValue);
+    if (lastWeekAttributeValue == playerAttributeValueNum) {
+      return 'growth-bg';
+    }
+
+    if (lastWeekAttributeValue < playerAttributeValueNum) {
+      return 'growth-bg growth-bg--up';
+    }
+
+    return 'growth-bg growth-bg--down';
+  }
 }
-
-//table.style.border = "1px solid black" i cell.style.padding = "4px"
-
-// data.forEach((rowData) => {
-//   const row: HTMLTableRowElement = tableRoot.insertRow();
-//   rowData.forEach((cellData) => {
-//     const cell: HTMLTableCellElement = row.insertCell();
-//     cell.textContent = cellData;
-//   });
-// });
-
-//growth-bg--up
-//growth-bg--down
